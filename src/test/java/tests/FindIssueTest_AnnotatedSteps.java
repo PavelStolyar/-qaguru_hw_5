67b213e94d34d903d05bc3c1989bc05bf369276a
pavelstolyar <stolyarps@gmail.com>
2021-04-01 01:04:43 +0300

    add annotation test;

---
+++ src/test/java/tests/FindIssueTest_AnnotatedSteps.java
@@ -0,0 +1,22 @@
+package tests;
+
+import org.junit.jupiter.api.Test;
+import webSteps.WebSteps;
+
+public class FindIssueTest_AnnotatedSteps {
+
+    private WebSteps steps = new WebSteps();
+
+    @Test
+    public void findIssueTest(){
+        //test data
+        String repositoryUrl = "https://github.com/PavelStolyar/-qaguru_hw_5/";
+        String tabName = "Issues";
+        String issueName = "TestIssue";
+
+        steps.openRepository(repositoryUrl);
+        steps.openTab(tabName);
+        steps.openIssueByName(issueName);
+        steps.checkIssueByName(issueName);
+    }
+}
---
+++ src/test/java/webSteps/WebSteps.java
@@ -0,0 +1,32 @@
+package webSteps;
+
+import io.qameta.allure.Step;
+
+import static com.codeborne.selenide.Condition.text;
+import static com.codeborne.selenide.Selectors.byText;
+import static com.codeborne.selenide.Selenide.$;
+import static com.codeborne.selenide.Selenide.open;
+
+public class WebSteps {
+
+    @Step ("Open {repositoryUrl}")
+    public void openRepository(String repositoryUrl)
+    {
+        open(repositoryUrl);
+    }
+
+    @Step ("Open {tabName} tab")
+    public void openTab(String tabName) {
+        $(byText(tabName)).click();
+    }
+
+    @Step ("Open {issueName} issue")
+    public void openIssueByName(String issueName) {
+        $(byText(issueName)).click();
+    }
+    @Step ("Check that {issueName} is opened")
+    public void checkIssueByName(String issueName) {
+        $(".js-issue-title.markdown-title").shouldHave(text(issueName));
+    }
+
+}
